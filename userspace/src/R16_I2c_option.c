434c26df21a133ba8154dad60f672746cc2b1ffb
oneface <jiangxj@xiaolj.com>
2018-03-14 14:19:35 +0800

    a little addtion

--- userspace/src/R16_I2c_option.c
+++ userspace/src/R16_I2c_option.c
@@ -19,16 +19,16 @@
 #include "Application_LED.h"
 
 static const BYTE *r16_i2c_node[] = {
+#ifdef R16_I2C0_LED
+	R16_I2C0_LED,
+#endif
+
 #ifdef R16_I2C1_LED
 	R16_I2C1_LED,
 #endif
 
 #ifdef R16_I2C2_LED
 	R16_I2C2_LED,
-#endif
-
-#ifdef R16_I2C3_LED
-	R16_I2C3_LED,
 #endif
 };
 
@@ -59,15 +59,54 @@ static void r16_i2c_set_handler_buf(R16_I2c_HANDLER **pp_r16_i2c_handler)
 /*
 	eg: reg[0x0, 0x01, 0x02, ...] to set data[0x12, 0x33, 0x42, ...]
  */
-/*
-   reg_access_data.device_commu = led_get_commu_group(aw981x_id);
-   reg_access_data.device_addr = led_get_i2c_address(aw981x_id);
-   reg_access_data.base_reg_addr = reg_addr;
-   reg_access_data.regs_num = reg_num;
-
- */
 void r16_i2c_writeregisters(BYTE mode, AWCHIP_REGS_ACCESS_STRUCT *reg_access_data, BYTE *p_value )
 {
+	int fd = -1, i;
+	BYTE retry = 2, ret = -1;
+	BYTE dcommu, base_reg, reg_num;
+	R16_I2c_HANDLER *p_i2c_handler = NULL;
+
+	if (NULL == reg_access_data) {
+		printf("reg_access_data is NULL\n");
+		return ;
+	}
+
+	dcommu = reg_access_data->device_commu;
+	p_i2c_handler = r16_i2c_get_handler_by_index(dcommu);
+	if (NULL != p_i2c_handler) {
+		fd = p_i2c_handler->fd;
+		(p_i2c_handler->i2c_data.msgs[0]).addr = reg_access_data->device_addr;
+		base_reg = reg_access_data->base_reg_addr;
+		reg_num = reg_access_data->regs_num;
+
+		(p_i2c_handler->i2c_data.msgs[0]).buf = (unsigned char *)malloc(2);
+		p_i2c_handler->i2c_data.nmsgs = 1;
+		(p_i2c_handler->i2c_data.msgs[0]).len = 1;
+		(p_i2c_handler->i2c_data.msgs[0]).flags = 0;
+		switch (mode) {
+			case EVENT_ID_AWCHIP_REG_READ_8BIT:
+				for (i = 0; i < reg_num; i++) {
+					while (retry > 0) {
+						(p_i2c_handler->i2c_data.msgs[0]).buf[0] = base_reg + i;
+						(p_i2c_handler->i2c_data.msgs[0]).buf[1] = *(p_value + i);
+						ret = ioctl(fd, I2C_RDWR, (unsigned long)p_i2c_handler);
+						if (ret < 0) {
+							perror("ioctl write time error");
+							retry--;
+							continue;
+						}
+						break;
+					}
+				}
+				break;
+				//EVENT_ID_AWCHIP_REG_READ_16BIT:
+				//unuse
+			default:
+				break;
+		}
+
+		free((p_i2c_handler->i2c_data.msgs[0]).buf);
+	}
 
 }
 
@@ -83,7 +122,6 @@ void r16_i2c_init()
 			if (-1 == pp_r16_i2c_handler[i]->fd) {
 				printf("open %s failed\n", r16_i2c_node[i]);
 			}
-			pp_r16_i2c_handler[i]->i2c_data = (struct i2c_rdwr_ioctl_data *)malloc(sizeof(struct i2c_rdwr_ioctl_data));
 		}
 	}
 
--- userspace/src/R16_I2c_option.h
+++ userspace/src/R16_I2c_option.h
@@ -9,6 +9,24 @@
 
 #include "Application_LED.h"
 
+#define I2C_RETRIES 0x0701  /* number of times a device address should
+							                      be polled when not acknowledging */
+#define I2C_TIMEOUT 0x0702  /* set timeout in units of 10 ms */
+/* NOTE: Slave address is 7 or 10 bits, but 10-bit addresses
+    * are NOT supported! (due to code brokenness)
+	 */
+#define I2C_SLAVE   0x0703  /* Use this slave address */
+#define I2C_SLAVE_FORCE 0x0706  /* Use this slave address, even if it
+								                      is already in use by a driver! */
+#define I2C_TENBIT  0x0704  /* 0 for 7 bit addrs, != 0 for 10 bit */
+#define I2C_FUNCS   0x0705  /* Get the adapter functionality mask */
+#define I2C_RDWR    0x0707  /* Combined R/W transfer (one STOP only) */
+#define I2C_PEC     0x0708  /* != 0 to use PEC with SMBus */
+#define I2C_SMBUS   0x0720  /* SMBus transfer */
+#define I2C_M_TEN 0x0010
+#define I2C_M_RD 0x0001
+
+
 #define R16_I2C0_LED			"/dev/i2c-0"
 #define R16_I2C1_LED			"/dev/i2c-1"
 #define R16_I2C2_LED			"/dev/i2c-2"
@@ -29,7 +47,7 @@ struct i2c_rdwr_ioctl_data {
 
 typedef struct {
 	int    fd;
-	struct i2c_rdwr_ioctl_data *i2c_data;
+	struct i2c_rdwr_ioctl_data i2c_data;
 }R16_I2c_HANDLER;
 
 
